2d5b3e679bf3e590a4c2cf8afe1d53051fe914b1
joseCardona01 <josesiprozmaster@gmail.com>
2024-04-20 04:01:20 +0200

    add section1 and section2

---
+++ taller-1-js/section2/app.js
@@ -0,0 +1,30 @@
+//Exercise four
+//Improve my habilities in the javascript.
+/* Obtain a good base in every concepts javascript, with us: Data types, variables, comments, improve 
+my logic and development many exercise. This is base are very important for the exercise next */
+
+//Exercise five
+let myNumber = 10;
+console.log(myNumber);
+
+//Exercise six
+let myBoolean = true;
+console.log(typeof(myBoolean));
+
+//Exercise seven
+let myString = "Hello world";
+console.log(myString.length);
+
+//Exercise eight
+let myArray = ["2",4,[]];
+console.log(myArray[2]);
+
+//Exercise nine
+const myObject = {
+    name: "jose",
+    age: 20,
+    email: "josesiprozmaster@gmail.com"
+    
+}
+
+console.log(myObject);
