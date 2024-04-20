2b9ff283ad4fc38f4da9e98558c63d78dce329ae
joseCardona01 <josesiprozmaster@gmail.com>
2024-04-20 14:30:51 +0200

    add the section five

---
+++ taller-1-js/section5/app.js
@@ -0,0 +1,70 @@
+
+
+//Section one ------------------------>
+//Exercise three
+/*This is a practice good, because the first that show is the html and ehe css. After the javascript. 
+the first Show the structure, after the styles and last the interactive (functions, process)*/
+alert("¡Bienvenido al taller de JavaScript!");
+
+//-------------------------------->
+
+//Section two ------------------->
+
+//Exercise four
+//Improve my habilities in the javascript.
+/* Obtain a good base in every concepts javascript, with us: Data types, variables, comments, improve 
+my logic and development many exercise. This is base are very important for the exercise next */
+
+//Exercise five
+let myNumber = 10;
+console.log(myNumber);
+
+//Exercise six
+let myBoolean = true;
+console.log(typeof(myBoolean));
+
+//Exercise seven
+let myString = "Hello world";
+console.log(myString.length);
+
+//Exercise eight
+let myArray = ["2",4,[]];
+console.log(myArray[2]);
+
+//Exercise nine
+const myObject = {
+    name: "jose",
+    age: 20,
+    email: "josesiprozmaster@gmail.com"
+    
+}
+
+console.log(myObject);
+
+//------------------------------->
+
+//Section three ------------------>
+//Exercise ten
+const enterNameUser = prompt("Ingrese el nombre");
+console.log(`Bienvenido ${enterNameUser}. Estamos contentos por ingresar!...`);
+
+//Exercise eleven
+const enterConfirmUser = confirm("Te gusta javascript"); 
+const confirmUser = (enterConfirmUser) ? "Te gusta Javascript": "No te gusta Javascript";
+console.log(`Tu valor confirmado fue ${confirmUser}`);
+
+//---------------------------------------------->
+
+//Section four ------------------>
+//Exercise nine
+console.warn("this is only a practice");
+
+//Exercise ten
+console.error("This is a error. Try again!.");
+
+//Exercise eleven
+
+const myArrayNumbers = [1,2,3,4,5];
+console.table(myArrayNumbers);
+
+//------------------------------------------------>
