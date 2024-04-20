96e4746348e7bffbab1bf52a05c1f43d16744755
joseCardona01 <josesiprozmaster@gmail.com>
2024-04-20 04:25:00 +0200

    add section three

---
+++ taller-1-js/section3/script.js
@@ -0,0 +1,8 @@
+//Exercise ten
+const enterNameUser = prompt("Ingrese el nombre");
+console.log(`Bienvenido ${enterNameUser}. Estamos contentos por ingresar!...`);
+
+//Exercise eleven
+const enterConfirmUser = confirm("Te gusta javascript"); 
+const confirmUser = (enterConfirmUser) ? "Te gusta Javascript": "No te gusta Javascript";
+console.log(`Tu valor confirmado fue ${confirmUser}`);
