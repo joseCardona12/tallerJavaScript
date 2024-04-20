4dd69bce4ebda0d3afad8d6527831582c8ff72ae
joseCardona01 <josesiprozmaster@gmail.com>
2024-04-20 14:20:36 +0200

    add a new section - section 4

---
+++ taller-1-js/section4/script.js
@@ -0,0 +1,10 @@
+//Exercise nine
+console.warn("this is only a practice");
+
+//Exercise ten
+console.error("This is a error. Try again!.");
+
+//Exercise eleven
+
+const myArrayNumbers = [1,2,3,4,5];
+console.table(myArrayNumbers);
